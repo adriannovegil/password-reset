37a1f093568b7b979a56cf395a471cfcbf669a09
Adrian Novegil <adrian.novegil@gft.com>
2016-07-01 16:59:08 -0500

    Added the login POST router.

--- routes/index.js
+++ routes/index.js
@@ -83,4 +83,17 @@ router.get('/login', function(req, res) {
   });
 });
 
+router.post('/login', function(req, res, next) {
+  passport.authenticate('local', function(err, user, info) {
+    if (err) return next(err)
+    if (!user) {
+      return res.redirect('/login')
+    }
+    req.logIn(user, function(err) {
+      if (err) return next(err);
+      return res.redirect('/');
+    });
+  })(req, res, next);
+});
+
 module.exports = router;
